e587ecceb17795fca0143eb907b82a79c3133458
Anton <apodvalnikov@softeqlab.com>
2024-10-20 02:50:00 +0300

    Rename swagger title

--- src/score_bel/settings/drf_spectacular.py
+++ src/score_bel/settings/drf_spectacular.py
@@ -12,8 +12,8 @@ env = environ.Env()
 IS_SWAGGER_ENABLED = env.bool('IS_SWAGGER_ENABLED', default=True)
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Kapusta API',
-    'DESCRIPTION': 'Kapusta API documentation',
+    'TITLE': 'ScoreBel API',
+    'DESCRIPTION': 'ScoreBel API documentation',
     'VERSION': '0.0.1',
     'SCHEMA_PATH_PREFIX': r'/v[0-9]',
     'COMPONENT_SPLIT_REQUEST': True
