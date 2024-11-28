58c4f90af2b09ccbc93e6a0039f3bfbd4823aef8
Anton <apodvalnikov@softeqlab.com>
2024-11-29 00:31:21 +0300

    Update db credential

--- src/score_bel/settings/databases.py
+++ src/score_bel/settings/databases.py
@@ -7,11 +7,11 @@ __all__ = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'railway'),
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'ieDmvMgTpPKcyZtFHmrtoiSFpnyGyDxM'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'junction.proxy.rlwy.net'),
-        'PORT': os.environ.get('POSTGRES_PORT', '26459'),
+        'NAME': os.environ.get('POSTGRES_DB', 'scorebel_bs1l'),
+        'USER': os.environ.get('POSTGRES_USER', 'scorebel'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 's1dBMyzJI72y3nmw1DrUe6nb3Dth7Hzs'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'dpg-ct4cn8d6l47c73f8ki90-a.frankfurt-postgres.render.com'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
         'TEST': {
             'NAME': 'test_score_bel_default',
         },
